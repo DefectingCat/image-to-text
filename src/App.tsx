f4a4f9803ab7f29539b9637a3c0c65526561ca88
DefectingCat <i@defect.ink>
2023-01-05 09:59:29 +0800

    Init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import clsx from 'clsx';
+import Button from 'components/Button';
+
+function App() {
+  return (
+    <>
+      <div className={clsx('p-4')}>
+        <Button>Hello</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,34 @@
+import clsx from 'clsx';
+import { ReactNode } from 'react';
+
+type ButtonProps = {
+  className?: string;
+  children?: ReactNode;
+};
+
+const Button = ({ className, children }: ButtonProps) => {
+  return (
+    <>
+      <button
+        className={clsx(
+          'bg-white border border-transparent hover:border-gray-200',
+          'outline-none hover:bg-gray-50 focus:ring-4 dark:border-transparent',
+          'focus:ring-cyan-200 font-medium rounded-lg text-sm',
+          'px-5 py-2.5 dark:bg-gray-800 dark:text-white ',
+          'dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:ring-cyan-800',
+          'transition-all disabled:hover:bg-gray-200',
+          'disabled:cursor-not-allowed disabled:dark:hover:bg-gray-700',
+          'disabled:hover:border-transparent',
+          'text-lg disabled:bg-gray-200 disabled:text-gray-500',
+          'dark:disabled:bg-gray-700 dark:disabled:text-gray-300',
+          'disabled:dark:hover:border-transparent',
+          className
+        )}
+      >
+        {children}
+      </button>
+    </>
+  );
+};
+
+export default Button;
